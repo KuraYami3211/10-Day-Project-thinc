c53b1a1a6fbfe414617732e0ef53c6a5051092e2
Nitipoom Pansayor <nitipoom2704@gmail.com>
2025-03-19 15:15:26 +0700

    fix contentselection

--- backend/contentSelectionAPI.js
+++ backend/contentSelectionAPI.js
@@ -10,13 +10,13 @@ app.use(express.json());
   
 
 const course = [
-  // { couseID:"2301107",courseName: "Cal I", department: "วิศวกรรมคอมพิวเตอร์(CP)", year: 1 },
-  // { couseID:"2301108",courseName: "Cal II", department: "วิศวกรรมคอมพิวเตอร์(CP)", year: 2 },
-  // { couseID:"2301103",courseName: "Gen Phy I", department: "วิศวกรรมคอมพิวเตอร์(CP)", year: 1 },
-  // { couseID:"2304104",courseName: "Gen Phy II", department: "วิศวกรรมคอมพิวเตอร์(CP)", year: 2 },
-  // { couseID:"2302127",courseName: "Gen Chem", department: "วิศวกรรมคอมพิวเตอร์(CP)", year: 2 },
-  // { couseID:"2110215",courseName: "Prog Meth", department: "วิศวกรรมคอมพิวเตอร์(CP)", year: 2 },
-  // { couseID:"2110221",courseName: "Com Eng Ess", department: "วิศวกรรมคอมพิวเตอร์(CP)", year: 2 },
+  { couseID:"2301107",courseName: "Cal I", department: "วิศวกรรมคอมพิวเตอร์(CP)", year: "1/1" },
+  { couseID:"2301108",courseName: "Cal II", department: "วิศวกรรมคอมพิวเตอร์(CP)", year: "1/2" },
+  { couseID:"2301103",courseName: "Gen Phy I", department: "วิศวกรรมคอมพิวเตอร์(CP)", year: "1/1" },
+  { couseID:"2304104",courseName: "Gen Phy II", department: "วิศวกรรมคอมพิวเตอร์(CP)", year: "1/2" },
+  { couseID:"2302127",courseName: "Gen Chem", department: "วิศวกรรมคอมพิวเตอร์(CP)", year: "1/2" },
+  { couseID:"2110215",courseName: "Prog Meth", department: "วิศวกรรมคอมพิวเตอร์(CP)", year: "1/2" },
+  { couseID:"2110221",courseName: "Com Eng Ess", department: "วิศวกรรมคอมพิวเตอร์(CP)", year: "1/2" },
   { couseID:"2110101",courseName: "Computer Programming", department: "วิศวกรรมคอมพิวเตอร์(CP)", year: "1/1" },
 ];
 
