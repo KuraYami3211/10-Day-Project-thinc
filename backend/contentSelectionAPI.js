edad5c4dbd165602a7dea081b303f723bbeeddca
Nitipoom Pansayor <nitipoom2704@gmail.com>
2025-03-20 15:01:19 +0700

    pull from prevrious commit

--- backend/contentSelectionAPI.js
+++ backend/contentSelectionAPI.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 const course = require("./data/courseInfo.js");
 const file = require("./data/file.js")
-const PORT = process.env.CONTENT_API_PORT || 6000;
+const port = 3000;
 
 app.use(express.static("frontend"));
 
@@ -25,30 +25,6 @@ app.post("/recommend", (req, res) => {
 
 app.get("/", (req, res) =>{
   res.sendFile(path.join(__dirname,"..","frontend","firsthome.html"))
-});
-
-
-app.post("/list-file", (req, res) => {
-  const { courseID, term } = req.body;
-
-  if (!courseID || !term) {
-    return res.status(400).json({ error: "กรุณากรอกภาควิชาและชั้นปี" });
-  }
-
-  // Find the course in the `file` array
-  const course = file.find((c) => c.courseID === courseID);
-
-  if (!course) {
-    return res.status(404).json({ error: "ไม่พบรหัสวิชานี้" });
-  }
-
-  if (term === "midterm") {
-    return res.json({ files: course.midterm });
-  } else if (term === "final") {
-    return res.json({ files: course.final });
-  } else {
-    return res.status(400).json({ error: "กรุณาระบุภาคการสอบให้ถูกต้อง (midterm หรือ final)" });
-  }
 });
 
 app.listen(PORT, () => {
